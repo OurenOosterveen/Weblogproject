fb1fc335b17e71786a5c61c41c9920dfca96bbda
OurenOosterveen <ouren.oosterveen@outlook.com>
2022-06-14 15:47:13 +0200

    Filled migrations and made a join table migration

--- Weblog/database/migrations/2022_06_14_131311_create_posts_table.php
+++ Weblog/database/migrations/2022_06_14_131311_create_posts_table.php
@@ -15,6 +15,10 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
+            $table->string('title');
+            $table->text('body');
+            $table->boolean('is_premium');
             $table->timestamps();
         });
     }
--- Weblog/database/migrations/2022_06_14_134531_create_categories_posts_table.php
+++ Weblog/database/migrations/2022_06_14_134531_create_categories_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesTable extends Migration
+class CreateCategoriesPostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,10 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('categories_posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->foreign('category_id');
+            $table->foreign('post_id');
             $table->timestamps();
         });
     }
@@ -27,6 +28,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('categories_posts');
     }
 }
