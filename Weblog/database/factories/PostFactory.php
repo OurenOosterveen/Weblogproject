b4d60cd2313a320c704161d64407088509c7f323
OurenOosterveen <ouren.oosterveen@outlook.com>
2022-06-16 11:18:28 +0200

    Created factories for user and post
    Created basic factories for user and posts and added relational connections between the two

--- Weblog/database/factories/PostFactory.php
+++ Weblog/database/factories/PostFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class PostFactory extends Factory
@@ -14,7 +15,11 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'title' => $this->faker->sentence(),
+            'body' => $this->faker->paragraph(),
+            'is_premium' => $this->faker->boolean(25)
+
         ];
     }
 }
