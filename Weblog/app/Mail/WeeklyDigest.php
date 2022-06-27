5375cf1b346f6bb258c85ffdda9e43051ec1df14
OurenOosterveen <ouren.oosterveen@outlook.com>
2022-06-27 15:05:00 +0200

    Premium mail safety

--- Weblog/app/Mail/WeeklyDigest.php
+++ Weblog/app/Mail/WeeklyDigest.php
@@ -3,6 +3,7 @@
 namespace App\Mail;
 
 use App\Models\Post;
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -13,7 +14,7 @@ class WeeklyDigest extends Mailable
 {
     use Queueable, SerializesModels;
 
-
+    public $posts;
 
     /**
      * Create a new message instance.
@@ -22,6 +23,18 @@ class WeeklyDigest extends Mailable
      */
     public function __construct()
     {
+        $user = User::where('email', '=', request('email'))->first();
+
+        if ($user->is_premium ?? false ){
+            $this->posts = Post::latest()
+                                ->where("created_at", ">=", Carbon::now()->subWeek()->toDateTimeString())
+                                ->get();
+        } else {
+            $this->posts = Post::latest()
+                                ->where("created_at", ">=", Carbon::now()->subWeek()->toDateTimeString())
+                                ->where('is_premium', '=', 'false')
+                                ->get();
+        }
         $this->afterCommit();
     }
 
@@ -32,8 +45,6 @@ class WeeklyDigest extends Mailable
      */
     public function build()
     {
-        return $this->markdown('mails.weeklyDigest', [
-            "posts" => Post::latest()->where("created_at", ">=", Carbon::now()->subWeek()->toDateTimeString())->get()
-        ]);
+        return $this->markdown('mails.weeklyDigest');
     }
 }
