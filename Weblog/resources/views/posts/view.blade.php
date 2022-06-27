c6f0bde4abcdc95528af8cf02a93358610d445b3
OurenOosterveen <ouren.oosterveen@outlook.com>
2022-06-27 15:36:59 +0200

    Update view.blade.php

--- Weblog/resources/views/posts/view.blade.php
+++ Weblog/resources/views/posts/view.blade.php
@@ -1,31 +1,37 @@
 <x-layout>
-    <div class="post">
-        <h4> {{ $post->title }}</h4>
-        <small>
-            Posted {{ $post->created_at->diffForHumans() }}
-            by {{ $post->user->username }}
-        </small>
+    @if ((auth()->user()->is_premium ?? false) || !$post->is_premium)
+        <div class="post">
+            <h4> {{ $post->title }}</h4>
+            <small>
+                Posted {{ $post->created_at->diffForHumans() }}
+                by {{ $post->user->username }}
+            </small>
+            <p>
+                {{ $post->body }}
+            </p>
+        </div>
+
+        <div class="comments">
+            <form action={{ route('comment.post', ['post' => $post->id]) }} method="post">
+                @csrf
+                <label for="comment">Write a comment</label> <br>
+                <textarea name="comment" id="comment" cols="30" rows="5"></textarea> <br>
+                <input type="submit" value="Post comment">
+            </form>
+
+            @foreach ($comments as $comment)
+                <div class="comment-card">
+                    <small>
+                        Posted {{ $comment->created_at->diffForHumans() }}
+                        by {{ $comment->user->username }}
+                    </small> <br>
+                    {{$comment->body}}
+                </div>
+            @endforeach
+        </div>
+    @else
         <p>
-            {{ $post->body }}
-        </p>
-    </div>
-
-    <div class="comments">
-        <form action={{ route('comment.post', ['post' => $post->id]) }} method="post">
-            @csrf
-            <label for="comment">Write a comment</label> <br>
-            <textarea name="comment" id="comment" cols="30" rows="5"></textarea> <br>
-            <input type="submit" value="Post comment">
-        </form>
-
-        @foreach ($comments as $comment)
-            <div class="comment-card">
-                <small>
-                    Posted {{ $comment->created_at->diffForHumans() }}
-                    by {{ $comment->user->username }}
-                </small> <br>
-                {{$comment->body}}
-            </div>
-        @endforeach
-    </div>
+            You are not authorised to view this content.
+            Please go back to our <a href={{ route('posts.index') }}>homepage</a> or become a <a href={{ route('posts.index') }}>premium member</a>.</p>
+    @endif
 </x-layout>
